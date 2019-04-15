759b39dcc333ad5f5c964378dd0dbef02fc28143
juw013 <juw013@ucsd.edu>
2019-04-15 15:20:08 +0000

    Lab1

--- Lab1_hello.v
+++ Lab1_hello.v
@@ -17,7 +17,7 @@ module Lab1_hello(input tb_a, input tb_b, input tb_c, output L1_andOut, output L
 	assign L1_orOut = tb_a | tb_b | tb_c;	// OR logic gate
 	assign L1_nandOut = ~(tb_a & tb_b & tb_c); // NAND logic gate
 	assign L1_norOut = ~(tb_a | tb_b | tb_c); // NOR logic gate
-	assign L1_notOut_a = ~tb_a; //NOT logic gate
+	assign L1_notOut_a = ~tb_a; //NOT logic gate 
 
 
 endmodule
